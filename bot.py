8519b7183272f23f5dffa3c6191a863d780ad5f9
Tonkoshkura Misha <tonkoshkura.misha@gmail.com>
2021-12-20 16:07:27 +0200

    my telegram anecdote bot

---
+++ bot.py
@@ -0,0 +1,21 @@
+import telebot
+import time
+
+# Токен, который выдает @botfather
+bot = telebot.TeleBot('5047953360:AAHEiTztBbd0R3WtIFPeNbZ871-X3QyIRZk')
+
+# Адрес телеграм-канала, начинается с @
+CHANNEL_NAME = '@My_100_Jokes_Bot'
+
+# Загружаем список шуток
+f = open('data/fun.txt', 'r', encoding='UTF-8')
+jokes = f.read().split('\n')
+f.close()
+
+# Пока не закончатся шутки, посылаем их в канал
+for joke in jokes:
+    bot.send_message(CHANNEL_NAME, joke)
+    # Делаем паузу в один час
+    time.sleep(3600)
+
+bot.send_message(CHANNEL_NAME, "Анекдоты закончились :-(")
